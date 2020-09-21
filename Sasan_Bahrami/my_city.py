5060de2a0fc839b424474aca574393b51533b50e
sasanbrhmcodes <sasan.bahrami.codes@gmail.com>
2020-09-21 13:43:46 +0330

    Sasan_Bahrami City Density

---
+++ Sasan_Bahrami/my_city.py
@@ -0,0 +1,10 @@
+
+
+
+city_name = 'Tehran'
+city_area = 730000
+city_population = 8694000
+city_density = city_population / city_area
+
+
+print('Population Density: ' + str(city_density) + ' inhabitants per hectar')
